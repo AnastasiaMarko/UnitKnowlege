e59c943485f9ee831007b7683ccfb0a4d8c1e64e
AnastasiaMarko <38351980+AnastasiaMarko@users.noreply.github.com>
2018-06-13 00:09:56 +0300

    Update connect.php

--- PHP/connect.php
+++ PHP/connect.php
@@ -9,7 +9,7 @@ global $DB_PASSWORD;
 
 
 $link = mysqli_connect( $DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_DATABASE)  // conecting with serwer
-    or die("Помилка підключення..." . mysqli_error($link));
+    or die("Помилка підключення #" .mysqli_connect_errno(). PHP_EOL);
 
 
 mysqli_close($link); // close connect
